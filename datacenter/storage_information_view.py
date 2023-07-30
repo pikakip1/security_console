89db1c5a1ae4dfdd8437d210376958fe4a62433d
pikakip1 <pikakip222@mail.ru>
2023-07-30 20:38:36 +0300

    Added output of people in the vault

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,36 @@
+from django.utils.timezone import localtime
+from datetime import datetime, timedelta
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    non_closed_visits = []
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    in_repository = Visit.objects.filter(leaved_at=None)
+    for person in in_repository:
+        duration = format_duration(get_duration(person))
+
+        visit = {
+            'who_entered': person.passcard.owner_name,
+            'entered_at': person.entered_at,
+            'duration': duration
         }
-    ]
+
+        non_closed_visits.append(visit)
+
     context = {
+
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
     return render(request, 'storage_information.html', context)
+
+
+def get_duration(vizit):
+    time_in_repository = str(localtime() - vizit.entered_at)
+    return time_in_repository
+
+
+def format_duration(duration):
+    return datetime.strptime(duration, '%H:%M:%S.%f').strftime('%H:%M')
