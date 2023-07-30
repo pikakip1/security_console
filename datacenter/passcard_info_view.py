ddfe595703b3433630c98c5e0bda99f3884b7da2
pikakip1 <pikakip222@mail.ru>
2023-07-30 20:39:31 +0300

    Added history of visits to the vault for each employee

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,32 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
+from datetime import timedelta
+from datacenter.storage_information_view import get_duration
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    person_visit = []
+    try:
+        person = Passcard.objects.get(passcode=passcode)
+    except Passcard.DoesNotExist:
+        raise Http404
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
-    context = {
-        'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
-    }
-    return render(request, 'passcard_info.html', context)
+    visits = Visit.objects.filter(passcard=person)
+
+    for visit in visits:
+        entered = visit.entered_at or get_duration(visit)
+        leaved = visit.leaved_at or localtime()
+        visit_person = {
+            'entered_at': entered,
+            'duration': leaved - entered,
+            'is_strange': (leaved - entered) > timedelta(hours=1)
+        }
+        person_visit.append(visit_person)
+
+    this_passcard_visits = {'passcard': person, 'this_passcard_visits': person_visit}
+    return render(request, 'passcard_info.html', this_passcard_visits)
+
