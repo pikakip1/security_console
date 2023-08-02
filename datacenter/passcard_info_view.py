bb13ec9e14c0fe42f33f8a29f18b80c0247d558b
pikakip1 <pikakip222@mail.ru>
2023-08-02 18:08:49 +0300

    Changed the method of calling the 404 error and shortened the code

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,29 +1,23 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils.timezone import localtime
 from datetime import timedelta
-from datacenter.storage_information_view import get_duration
-from django.http import Http404
+from datacenter.format_duration import get_duration
 from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    person_visit = []
-    try:
-        person = Passcard.objects.get(passcode=passcode)
-    except Passcard.DoesNotExist:
-        raise Http404
 
+    person_visit = []
+    person = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=person)
 
     for visit in visits:
-        entered = visit.entered_at or get_duration(visit)
-        leaved = visit.leaved_at or localtime()
+
         visit_person = {
-            'entered_at': entered,
-            'duration': leaved - entered,
-            'is_strange': (leaved - entered) > timedelta(hours=1)
+            'entered_at': visit.entered_at,
+            'duration': get_duration(visit),
+            'is_strange': get_duration(visit) > timedelta(hours=1)
         }
         person_visit.append(visit_person)
 
